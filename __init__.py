0ba059a2aa4483114f76a5880e45b88faa6f2358
Pascal Spörri <pascal@moeeeep.com>
2011-12-31 21:57:10 +0100

    Changed Flickr Interface...

--- __init__.py
+++ __init__.py
@@ -12,21 +12,11 @@ app = Flask(__name__)
 app.static_path="static"
 app.static_url_path="static/"
 
-f = flickr.Flickr(config.FLICKR_URL)
-f.loadPhotos()
-json_photos = []
-for photo in f.photos:
-  r = {}
-  r["title"] = photo["title"]
-  r["latitude"] = photo["latitude"]
-  r["longitude"] = photo["longitude"]
-  r["content"] = u"""<a href="{link}/lightbox/" target="_blank"><img src="{thumbnail}" alt="{title}"></a> <b>{title}</b>""".format(
-      link=photo["photo_url"], thumbnail=photo["thumbnail"], title=photo["title"])
-  json_photos.append(r)
+f = flickr.Flickr(config.FLICKR_USER)
 
 @app.route("/json")
 def return_photos():
-    return json.dumps({"markers": json_photos})
+    return json.dumps({"markers": f.toJson()})
 
 @app.route("/")
 def main():
@@ -34,4 +24,5 @@ def main():
     return template.render(title=config.TITLE, google_api_key=config.GOOGLE_MAPS_KEY)
 
 if __name__ == "__main__":
-    app.run(port=6000,host="0.0.0.0")
+#    app.debug = True
+    app.run(port=15000)
--- flickr.py
+++ flickr.py
@@ -1,61 +1,64 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from config import FLICKR_URL
-import flickr_api
+from config import FLICKR_USER, FLICKR_API_KEY
+import flickrapi
+import json
 
 class Flickr():
-  def __init__(self, url):
-    self.user = flickr_api.Person.getByUrl(url)
-    self.photos = None
-  
-  def getInfo(self, photo):
-    exif = photo.getExif()
-    try:
-      gps = photo.getLocation()
-    except flickr_api.base.FlickrAPIError:
-      return None
-
-    r = {}
-    r["id"] = photo.id
-    r["title"] = photo.title
-    r["thumbnail"] = photo.getPhotoFile("Square")
-    r["photo_url"] = photo.getPageUrl()
-    r["longitude"] = gps.longitude
-    r["latitude"] = gps.latitude
-    r["location_accuracy"] = gps.accuracy
-
-    for e in exif:
-      tag = e["tag"]
-      raw = e["raw"]
-      if tag == "Model":
-        r["camera"] = raw
-      if tag == "Lens":
-        r["lens"] = raw
-      if tag == "FNumber":
-        r["fnumber"] = raw
-      if tag == "ISO":
-        r["iso"] = raw
-      if tag == "FocalLength":
-        r["focallength"] = raw
-    return r
-
-  def loadPhotos(self):   
+  def __init__(self, user_id):
+    self.flickr = flickrapi.FlickrAPI(FLICKR_API_KEY, format='json')
+    self.flickr.cache = flickrapi.SimpleCache(timeout=300, max_entries=200)
+    self.user_id = user_id
+    self.loadPhotos()
+    self.json_dump = None
+  def loadPhotos(self):
+    print "Loading photos for user {user}".format(user=self.user_id)
+    
     page = 1
     photos = []
     while True:
-      res = self.user.getPublicPhotos(page=page,per_page=200)
-      photos += res.data
-#break
-      if page >= res.info.get("pages"):
-        break
-      page += 1
-      """
-      break
-      """
-    # Add exif
-    self.flickrobj = photos
-    self.photos = filter(lambda l: l != None, map(lambda l: self.getInfo(l), photos))
+        print "Page", page
+        result = self.flickr.people_getPublicPhotos(user_id=self.user_id, page=page, extras="geo,icon_server,description,url_sq, url_t, url_s, url_m, url_z, url_l,path_alias")
+#        print result[14:-1]
+        j = json.loads(result[14:-1])
+        photos += j["photos"]["photo"]
+        current_page = j["photos"]["page"]
+        if current_page >= j["photos"]["pages"]:
+          break
+    
+        page += 1
+    self.f_photos = photos 
+    self.photos = map(lambda l: self.getInfo(l), self.f_photos)
+
+  def getFlickrPage(self, photo):
+    return "http://www.flickr.com/photos/{user}/{photo_id}".format(
+        user=photo["pathalias"], photo_id=photo["id"])
+  
+  def getInfo(self, photo):
+    r = {}
+    r["id"] = photo["id"]
+    r["title"] = photo["title"]
+    r["thumbnail"] = photo["url_sq"]
+    r["photo_url"] = self.getFlickrPage(photo)+"/lightbox/"
+    r["longitude"] = photo["longitude"]
+    r["latitude"] = photo["latitude"]
+    r["location_accuracy"] = photo["accuracy"]
+    return r
+
+  def toJson(self):
+    if self.json_dump == None:
+      json_photos = []
+      for photo in self.photos:
+        r = {}
+        r["title"] = photo["title"]
+        r["latitude"] = photo["latitude"]
+        r["longitude"] = photo["longitude"]
+        r["content"] = u"""<a href="{link}/lightbox/" target="_blank"><img src="{thumbnail}" alt="{title}"></a> <b>{title}</b>""".format(
+            link=photo["photo_url"], thumbnail=photo["thumbnail"], title=photo["title"])
+        json_photos.append(r)
+      self.json_dump = json.dumps(json_photos)
+    return self.json_dump
 
 if __name__ == "__main__": 
-  f = Flickr(FLICKR_URL)
-  f.loadPhotos()
+  f = Flickr(FLICKR_USER)
+  print f.photos
